d54d87ea473f38f66580e788d909cc2b8f0f3372
matheusgb <matheusgbr98@gmail.com>
2023-04-18 21:07:15 -0300

    refactor: creates an index file in the protocols folder to improve the import of protocols in tests and the rest of the application

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,8 +1,8 @@
-import { type HttpResponse, type HttpRequest } from '../protocols/http'
-import { MissingParamError, InvalidParamError } from '../errors'
-import { badRequest, serverError } from '../helpers/http-helper'
-import { type Controller } from '../protocols/controller'
-import { type EmailValidator } from '../protocols/email-validator'
+import { type HttpResponse, type HttpRequest, type Controller, type EmailValidator } from '../protocols'
+import { MissingParamError } from '../errors/missing-param-error'
+import { InvalidParamError } from '../errors/invalid-param-error'
+import { ServerError } from '../errors/server-error'
+import { badRequest } from '../helpers/http-helper'
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator
 
@@ -27,7 +27,10 @@ export class SignUpController implements Controller {
         body: 'ok'
       }
     } catch (error) {
-      return serverError()
+      return {
+        statusCode: 500,
+        body: new ServerError()
+      }
     }
   }
 }
