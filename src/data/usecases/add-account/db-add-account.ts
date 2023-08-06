ef4d166347cbe0bb410cfe977db0cdaea451f290
matheusgb <matheusgbr98@gmail.com>
2023-08-06 19:29:03 -0300

    feat: create encrypter protocols and db-add-account usecase

---
+++ src/data/usecases/add-account/db-add-account.ts
@@ -0,0 +1,16 @@
+import { type AddAccount, type AddAccountModel } from '../../../domain/usecases/add-account/add-account'
+import { type AccountModel } from '../../../domain/models/account'
+import { type Encrypter } from '../../protocols/encrypter'
+
+export class DbAddAccount implements AddAccount {
+  private readonly encrypter: Encrypter
+
+  constructor (encrypter: Encrypter) {
+    this.encrypter = encrypter
+  }
+
+  async add (account: AddAccountModel): Promise<AccountModel | any> {
+    await this.encrypter.encrypt(account.password)
+    return await new Promise(resolve => { resolve(null) })
+  }
+}
