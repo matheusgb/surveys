d51b036049d428bde917f422effe4f21a22f9078
matheusgb <matheusgbr98@gmail.com>
2023-08-12 13:18:07 -0300

    feat: ensure DbAddAccount calls AddAccountRepository with correct values

---
+++ src/data/protocols/add-account-repository.ts
@@ -0,0 +1,6 @@
+import { type AccountModel } from '../../domain/models/account'
+import { type AddAccountModel } from '../../domain/usecases/add-account/add-account'
+
+export interface AddAccountRepository {
+  add: (accountData: AddAccountModel) => Promise<AccountModel>
+}
