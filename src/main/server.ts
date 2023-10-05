2b891ba5060d99f6418ac0b432e2ed54959df923
matheusgb <matheusgbr98@gmail.com>
2023-10-05 14:29:30 -0300

    feat: ensure requests parse body as json

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,4 +1,3 @@
-import express from 'express'
+import app from './config/app'
 
-const app = express()
-app.listen(3000, () => { console.log('Server is running') })
+app.listen(3001, () => { console.log('Server is running') })
