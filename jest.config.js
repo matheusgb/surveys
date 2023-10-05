96b0686d8a0d8281da5ac53f0ffc2f5fa9ca899e
matheusgb <matheusgbr98@gmail.com>
2023-10-05 20:27:02 -0300

    fix: adjust jest coverage file

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,14 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**'
+  ],
   coverageDirectory: 'coverage',
   preset: '@shelf/jest-mongodb',
   coveragePathIgnorePatterns: [
     'index.ts',
-    '.*-protocols.ts$',
-    '<rootDir>/src/main/**'
+    '.*-protocols.ts$'
   ],
   transform: {
     '.+\\.ts$': 'ts-jest'
