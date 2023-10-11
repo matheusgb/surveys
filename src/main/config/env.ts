f37adcf27bb10308b715a782e9500f84383e6e2b
matheusgb <matheusgbr98@gmail.com>
2023-10-10 22:04:44 -0300

    feat: add LogMongoRepository to signup factory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,4 @@
 export default {
-  mongoUrl: global.__MONGO_URI__ ?? 'mongodb://localhost:27017/clean-node-api',
+  mongoUrl: global.__MONGO_URI__ ?? 'mongodb://localhost:27017/survey-api',
   port: process.env.PORT ?? 3001
 }
